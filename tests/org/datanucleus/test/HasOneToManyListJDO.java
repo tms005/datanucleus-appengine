620cefc56e815b3b35eeaea0e380f235bd4d6518
max.ross <max.ross@ea1b383e-165b-11de-8120-8d224146dbbc>
2009-03-18 17:10:36 +0000

    Cherrypicked from HEAD
    Several important bug fixes.
    
    1) Fix automatic entity group assignment with non-transactional datasources.
    
    2) Support aliased table names.
    
    3) Fix getSingleResult() (jpa)
    
    4) Support filtering with transient params

--- tests/org/datanucleus/test/HasOneToManyListJDO.java
+++ tests/org/datanucleus/test/HasOneToManyListJDO.java
@@ -30,7 +30,7 @@ import javax.jdo.annotations.PrimaryKey;
 /**
  * @author Max Ross <maxr@google.com>
  */
-@PersistenceCapable(identityType = IdentityType.APPLICATION)
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable = "true")
 public class HasOneToManyListJDO implements HasOneToManyJDO {
 
   @PrimaryKey
